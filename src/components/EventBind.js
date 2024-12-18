c390c2304b393e3742e0f0c0fff71f66930ea21a
Shriii04 <sheehatyalikar10@gmail.com>
2024-12-18 15:16:21 +0530

    Wed

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -7,20 +7,27 @@ class EventBind extends Component {
       this.state = {
          message : 'Hello'
       }
+    //   this.clickHandler = this.clickHandler.bind(this)
     }
 
-    clickHandler(){
-        this.setState({
-            message : 'GoodBye'
-        })
+    // clickHandler(){
+    //     this.setState({
+    //         message : 'GoodBye'
+    //     })
         
+    // }
+    clickHandler = () => {
+        this.setState({
+            message : "GoodBye"
+        })
     }
   render() {
     return (
       <div>
         <div>{this.state.message}</div>
         {/* <button onClick={this.clickHandler.bind(this)}>Click</button> */}
-        <button onClick={() => this.clickHandler()}>Click</button>
+        {/* <button onClick={() => this.clickHandler()}>Click</button> */}
+        <button onClick={this.clickHandler}>Click</button>
       </div>
     )
   }
